0724473b7ef09bc2cda8aebbe808013e869bfd5a
Jacob Van Dyke <jacobvandyke194@gmail.com>
2023-02-08 21:44:32 -0700

    finished adding styling

--- server.js
+++ server.js
@@ -1,16 +1,13 @@
 const path = require('path');
+const helpers = require('./utils/helpers')
+const routes = require('./controllers');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-const helpers = require('./utils/helpers')
-const routes = require('./controllers');
-
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
 const app = express();
 const PORT = process.env.PORT || 3001;
-
 const hbs = exphbs.create({ helpers });
 
 const sess = {
@@ -40,5 +37,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log('Now listening on localhost:3001'));
 });
